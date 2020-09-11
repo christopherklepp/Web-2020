78dedef8a6aa00e36df431c2f4f543462cbef293
EivindA <98eivindaskeland@gmail.com>
2020-09-11 11:31:52 +0200

    9

--- Web2020/Web2020/wwwroot/js/lagre.js
+++ Web2020/Web2020/wwwroot/js/lagre.js
@@ -1,13 +1,21 @@
-﻿function lagreBestilling () {
+﻿function lagreBestilling() {
     const buss = {
-        fraReise: $("#reiseFra").val(),
-        tilReise: $("#reiseTil").val(),
+        reiserFra: $("#reiseFra").val(),
+        reiserTil: $("#reiseTil").val(),
         tidspunkt: $("#tidspunkt").val(),
         fornavn: $("#fornavn").val(),
         etternavn: $("#etternavn").val(),
         adresse: $("#adresse").val(),
         telefonnr: $("#telefonnr").val()
     };
+    console.log(buss.reiserFra);
+    console.log(buss.reiserTil);
+    console.log(buss.tidspunkt);
+    console.log(buss.fornavn);
+    console.log(buss.etternavn);
+    console.log(buss.adresse);
+    console.log(buss.telefonnr);
+
     const url = "buss/SettInnData";
     $.post(url, buss, function () {
         window.location.href = "bekreftelse.html";
