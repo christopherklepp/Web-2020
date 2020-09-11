ab0652a27dfbf7c9e79df77753cd09f0b45bd8d0
EivindA <98eivindaskeland@gmail.com>
2020-09-11 10:44:06 +0200

    8

---
+++ Web2020/Web2020/wwwroot/js/lagre.js
@@ -0,0 +1,15 @@
+﻿function lagreBestilling () {
+    const buss = {
+        fraReise: $("#reiseFra").val(),
+        tilReise: $("#reiseTil").val(),
+        tidspunkt: $("#tidspunkt").val(),
+        fornavn: $("#fornavn").val(),
+        etternavn: $("#etternavn").val(),
+        adresse: $("#adresse").val(),
+        telefonnr: $("#telefonnr").val()
+    };
+    const url = "buss/SettInnData";
+    $.post(url, buss, function () {
+        window.location.href = "bekreftelse.html";
+    });
+}
