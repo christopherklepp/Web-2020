3526a60f53ec1b046c2f7a69938063ce98599c8f
vickysmalls <55132051+vickysmalls@users.noreply.github.com>
2020-09-25 12:46:43 +0200

    js kvitering

--- Web2020/Web2020/wwwroot/js/bekreftelse.js
+++ Web2020/Web2020/wwwroot/js/bekreftelse.js
@@ -1,10 +1,24 @@
 ﻿$(function () {
     $.get("buss/SisteBestilling", function (buss) {
+        let ut = "<div>"
+        ut +=  "<label>Fornavn:  </label>" + buss.fornavn  + "<label>Etternavn:  </label>" + buss.etternavn + "</br>"
+        ut +=  "<label>Adresse:  </label>" + buss.adresse + "<label>Telefonnr:  </label>" + buss.telefonnr + "</br>"
+        ut += "<label>Tidspunkt:  </label>" + buss.tidspunkt + "<label>Til:  </label>" + buss.reiserTil+ "<label>Fra:  </label>"+ buss.reiserFra 
+        ut += " </div>";
+
+        $("#bekreftelse").html(ut);
+    });
+});
+
+/*$(function () {
+    $.get("buss/SisteBestilling", function (buss) {
         let ut = "<table> <tr>"
-        ut += "<td>" + buss.fornavn + "</td>" + "<td>" + buss.etternavn + "</td>" + "<td>" + buss.adresse + "</td>" + "<td>" + buss.telefonnr + "</td>";
-        ut += "<td>" + buss.tidspunkt + "</td>" + "<td>" + buss.reiserTil + "</td>" + "<td>" + buss.reiserFra + "</td>"
+        ut += "<td>" + "<label>Fornavn</label>" + buss.fornavn + "</td>" + "<td>" + "<label>Etternavn</label>" + buss.etternavn + "</td></br>"
+        ut += "<td>" + "<label>Adresse</label>" + buss.adresse + "</td>" + "<td>" + "<label>Telefonnr</label>" + buss.telefonnr + "</td></br>"
+        ut += "<td>" + "<label>Tidspunkt</label>" + buss.tidspunkt + "</td>" + "<td>" + "<label>Fra</label>" + buss.reiserTil + "</td>" + "<td>" + buss.reiserFra + "</td>"
         ut += "</tr> </table>";
 
         $("#bekreftelse").html(ut);
     });
 });
+*/
