d994e335a91c689b054df4da5f43b65cca300b43
vickysmalls <55132051+vickysmalls@users.noreply.github.com>
2020-09-25 11:14:21 +0200

    Regex greier i bussklassen
    Regex greier i bussklassen

--- Web2020/Web2020/Models/Buss.cs
+++ Web2020/Web2020/Models/Buss.cs
@@ -19,16 +19,17 @@ namespace Web2020.Models
         [DataType(DataType.Date)]
         public DateTime tidspunkt { get; set; }
         [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
-        [StringLength(20)]
+        [StringLength(20, ErrorMessage = "{0} lengden må være mellom {2} og {1}.", MinimumLength = 2)]
         public string fornavn { get; set; }
         [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
-        [StringLength(20)]
+        [StringLength(20, ErrorMessage = "{0} lengden må være mellom {2} og {1}.", MinimumLength = 2)]
         public string etternavn { get; set; }
         [RegularExpression(@"^[A-Z]+[a-zA-Z0-9]*$")]
-        [StringLength(20)]
+        [StringLength(50, ErrorMessage = "{0} lengden må være mellom {2} og {1}.", MinimumLength = 2)]
         public string adresse { get; set; }
-        /*[RegularExpression(@"^[2-9]\d{7}*$")]
-        [StringLength(20)]*/
+        [RegularExpression(@"^\w+@\w+\.\w{2,3}$")] 
+        [StringLength(20, ErrorMessage = "{0} lengden må være mellom {2} og {1}.", MinimumLength = 2)]
+        public string epost { get; set; }
         public string telefonnr { get; set; }
     }
 }
