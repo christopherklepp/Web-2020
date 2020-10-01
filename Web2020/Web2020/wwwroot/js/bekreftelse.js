a35a4be405c9d731db68e91b1e48e9e033218720
EivindA <98eivindaskeland@gmail.com>
2020-10-01 10:12:49 +0200

    Fjernet main.js

--- Web2020/Web2020/wwwroot/js/bekreftelse.js
+++ Web2020/Web2020/wwwroot/js/bekreftelse.js
@@ -1,4 +1,6 @@
-﻿$(function () {
+﻿//Skriver ut kvitering
+
+$(function () {
     $.get("buss/SisteBestilling", function (buss) {
         let ut = "<div>"
         ut += "<label>Fornavn:  </label> " + buss.fornavn + "</br>" +" <label>Etternavn:  </label> " + buss.etternavn + "</br>"
@@ -10,16 +12,3 @@
         $("#bekreftelse").html(ut);
     });
 });
-
-/*$(function () {
-    $.get("buss/SisteBestilling", function (buss) {
-        let ut = "<table> <tr>"
-        ut += "<td>" + "<label>Fornavn</label>" + buss.fornavn + "</td>" + "<td>" + "<label>Etternavn</label>" + buss.etternavn + "</td></br>"
-        ut += "<td>" + "<label>Adresse</label>" + buss.adresse + "</td>" + "<td>" + "<label>Telefonnr</label>" + buss.telefonnr + "</td></br>"
-        ut += "<td>" + "<label>Tidspunkt</label>" + buss.tidspunkt + "</td>" + "<td>" + "<label>Fra</label>" + buss.reiserTil + "</td>" + "<td>" + buss.reiserFra + "</td>"
-        ut += "</tr> </table>";
-
-        $("#bekreftelse").html(ut);
-    });
-});
-*/
