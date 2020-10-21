ec62bea8e107b499a8bd347e7df9d9238c7ebcb3
vickysmalls <55132051+vickysmalls@users.noreply.github.com>
2020-10-21 11:10:24 +0200

    Code Coverage

--- Web2020/Web2020Test/BussControllerTest.cs
+++ Web2020/Web2020Test/BussControllerTest.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -12,10 +16,19 @@ namespace Web2020Test
 {
     public class BussControllerTest
     {
+        private const string _loggetInn = "loggetInn";
+        private const string _ikkeLoggetInn = "";
+
+        private readonly Mock<IBussRepository> mockRep = new Mock<IBussRepository>();
+        private readonly Mock<ILogger<BussController>> mockLog = new Mock<ILogger<BussController>>();
+
+        private readonly Mock<HttpContext> mockHttpContext = new Mock<HttpContext>();
+        private readonly MockHttpSession mockSession = new MockHttpSession();
+
         [Fact]
-        public async Task SettInnData()
+        public async Task SettInnDataLoggetInnOK()
         {
-            //Arrange
+            /*
             var innBuss = new Buss()
             {
                 Id = 1,
@@ -26,19 +39,25 @@ namespace Web2020Test
                 etternavn = "Kristiansen",
                 epost = "ola1999@gmail.com",
                 pris = 299
-            };
+            };*/
 
-            var mock = new Mock<IBussRepository>();
-            var loggerMock = new Mock<ILogger<BussController>>();
-            mock.Setup(k => k.SettInnData(innBuss)).ReturnsAsync(true);
-            var bussController = new BussController(mock.Object, loggerMock.Object);
+            //Arrange
+            
+            mockRep.Setup(b => b.SettInnData(It.IsAny<Buss>())).ReturnsAsync(true);
+            var bussController = new BussController(mockRep.Object, mockLog.Object);
+
+            mockSession[_loggetInn] = _loggetInn;
+            mockHttpContext.Setup(s => s.Session).Returns(mockSession);
+            bussController.ControllerContext.HttpContext = mockHttpContext.Object;
 
 
             //act
-            ActionResult returOK = await bussController.SettInnData(innBuss);
+            var resultat = await bussController.SettInnData(It.IsAny<Buss>()) as OkObjectResult;
 
             //assert
-            //Assert.True(returOK);
+
+            Assert.Equal((int)HttpStatusCode.OK, resultat.StatusCode);
+            Assert.Equal("Bestilling lagretttt", resultat.Value);
         }
     }
 }
